292713be5e209fa4cedc04d717eb2811b32c668a
Lukas Zapletal <lzap+git@redhat.com>
2025-05-06 16:22:32 +0200

    Reformat code with shfmt

--- release.sh
+++ release.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -xe
+shfmt -w git-xcleaner
 tito build --tgz
 mv /tmp/tito/git-xcleaner*tar.gz /home/lzap/Scratch/lzap.fedorapeople.org/public_html/projects/git-xcleaner
 /home/lzap/Scratch/upload
